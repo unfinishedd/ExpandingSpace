4a943742276ddde1823e0682ea76cb37194f7457
Daniel <danielnijkamp2004@gmail.com>
2021-05-18 14:02:42 +0200

    Added background script and reworked UI

---
+++ Space/Assets/Scripts/BackgroundScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScript : MonoBehaviour
+{
+    
+    public float foo;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", foo += 1 * Time.deltaTime);
+        
+    }
+   
+}
