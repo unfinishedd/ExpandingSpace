094858e82d53de5d3f9222273e78f12155a54de2
Daniel <danielnijkamp2004@gmail.com>
2021-06-13 16:56:46 +0200

    Changed movement settings and added playermodel

--- Space/Assets/Scripts/MovementVisual.cs
+++ Space/Assets/Scripts/MovementVisual.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class MovementVisual : MonoBehaviour
 {
     public Rigidbody ball;
-    private float _lerpSpeed = 5f;
+    private float lerpSpeed = 5f;
 
     // Start is called before the first frame update
     void Start()
@@ -14,14 +14,14 @@ public class PlayerMovement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(ball.velocity, Vector3.up), Time.deltaTime * _lerpSpeed);
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(ball.velocity, Vector3.up), Time.deltaTime * lerpSpeed);
         transform.position = ball.transform.position + Vector3.up * Mathf.Sin(Time.deltaTime);
     }
 
     protected void OnDrawGizmos()
     {
-        Gizmos.DrawLine(ball.transform.position, ball.transform.position + ball.velocity);
+        Gizmos.DrawLine(ball.transform.position, Vector3.up + ball.velocity);
     }
 }
